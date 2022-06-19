109dcbf8a194e5d7a7efc49c5338bd79f2e04f04
KiriKaz <igkepsi@gmail.com>
2022-06-19 19:55:51 -0300

    feat: hide text lines before they get displayed

--- src/App.tsx
+++ src/App.tsx
@@ -12,17 +12,17 @@ const App = () => {
   useEffect(() => {
     tl.current = gsap.timeline()
       .to(header1.current, { duration: 0.5, marginTop: 0, opacity: 1 })
-      .to(header2.current, { delay: 0.5, duration: 1, opacity: 1 })
+      .to(header2.current, { delay: 0.5, duration: 1, opacity: 1, visibility: 'visible' })
       .to(header1.current, { duration: 1, marginTop: -30 }, "<")
-      .to(header3.current, { delay: 0.5, duration: 1, opacity: 1 })
+      .to(header3.current, { delay: 0.5, duration: 1, opacity: 1, visibility: 'visible' })
       .to(header1.current, { duration: 1, marginTop: -60 }, "<");
   }, [])
 
   return (
     <div tw="text-center flex flex-col justify-center h-full">
      <h1 ref={header1} tw="text-3xl sm:text-4xl md:text-5xl font-extrabold text-blue-100 mb-5 mt-60 opacity-0">Hello there!</h1>
-     <h2 ref={header2} tw="text-2xl sm:text-3xl md:text-4xl font-light italic text-gray-300 mb-3 opacity-0">We're still working on this at the moment.</h2>
-     <h2 ref={header3} tw="text-2xl sm:text-3xl md:text-4xl font-light italic text-gray-300 opacity-0">Check back <span tw="hover:text-yellow-100">soon</span> for <a href="https://github.com/KiriKaz" tw="text-blue-400 font-semibold">qkjrmid</a>'s portfolio!</h2>
+     <h2 ref={header2} tw="text-2xl sm:text-3xl md:text-4xl font-light italic text-gray-300 invisible opacity-0">We're still working on this at the moment.</h2>
+     <h2 ref={header3} tw="text-2xl sm:text-3xl md:text-4xl font-light italic text-gray-300 invisible opacity-0">Check back <span tw="hover:text-yellow-100">soon</span> for <a href="https://github.com/KiriKaz" tw="text-blue-400 font-semibold">qkjrmid</a>'s portfolio!</h2>
     </div>
   );
 }
